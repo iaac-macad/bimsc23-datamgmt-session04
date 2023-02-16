a5d5deca2040bb9fb913d148cdcdcde43695b973
rayharli <122750416+ray-harli@users.noreply.github.com>
2023-02-16 10:40:46 +0200

    JS update

--- ray/script.js
+++ ray/script.js
@@ -25,10 +25,10 @@ rhino3dm().then(async m => {
     rhino = m // global
 
 
-    //RhinoCompute.url = getAuth( 'RHINO_COMPUTE_URL' ) // RhinoCompute server url. Use http://localhost:8081 if debugging locally.
-    //RhinoCompute.apiKey = getAuth( 'RHINO_COMPUTE_KEY' )  // RhinoCompute server api key. Leave blank if debugging locally.
+    RhinoCompute.url = getAuth( 'http://35.157.191.153/' ) // RhinoCompute server url. Use http://localhost:8081 if debugging locally.
+    RhinoCompute.apiKey = getAuth( 'macad2023' )  // RhinoCompute server api key. Leave blank if debugging locally.
     
-    RhinoCompute.url = 'http://localhost:8081/' //if debugging locally.
+    // RhinoCompute.url = 'http://localhost:8081/' //if debugging locally.
 
 
     // load a grasshopper file!
